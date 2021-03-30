97393bfd6249f2e615effe6f2c97c0f73b2301ae
0xCAFEDECAF <31759302+0xCAFEDECAF@users.noreply.github.com>
2021-03-30 14:12:04 +0200

    Fixed typo

--- examples/VanBusDump/VanBusDump.ino
+++ examples/VanBusDump/VanBusDump.ino
@@ -75,7 +75,7 @@ void setup()
     Serial.begin(115200);
     Serial.println("Starting VAN bus receiver");
 
-    // Disable WIFI altogether to get rid of long and variable interrupt latency, causing packet CRC errors
+    // Disable Wi-Fi altogether to get rid of long and variable interrupt latency, causing packet CRC errors
     // From: https://esp8266hints.wordpress.com/2017/06/29/save-power-by-reliably-switching-the-esp-wifi-on-and-off/
     WiFi.disconnect(true);
     delay(1); 
