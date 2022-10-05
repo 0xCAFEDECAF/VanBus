2a44bc636a6049676ea37abd48773f9891c5032b
0xCAFEDECAF <31759302+0xCAFEDECAF@users.noreply.github.com>
2022-10-05 15:40:18 +0200

    Prepare for release 0.3.1

--- VanBusVersion.h
+++ VanBusVersion.h
@@ -3,7 +3,7 @@
  *
  * Written by Erik Tromp
  *
- * Version 0.3.0 - September, 2022
+ * Version 0.3.1 - October, 2022
  *
  * MIT license, all text above must be included in any redistribution.
  */
@@ -11,13 +11,13 @@
 #ifndef VanBusVersion_h
 #define VanBusVersion_h
 
-#define VAN_BUS_VERSION "0.3.0"
+#define VAN_BUS_VERSION "0.3.1"
 
 #define VAN_BUS_VERSION_MAJOR 0
 #define VAN_BUS_VERSION_MINOR 3
-#define VAN_BUS_VERSION_PATCH 0
+#define VAN_BUS_VERSION_PATCH 1
 
-#define VAN_BUS_VERSION_INT 000003000
+#define VAN_BUS_VERSION_INT 000003001
 
 #define VAN_BUS_RX_VERSION VAN_BUS_VERSION_INT
 
